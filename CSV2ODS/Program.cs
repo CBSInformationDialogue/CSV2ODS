f8729a972003f884ad91e1513a877b03bfaca196
Daniel Xu <sxu.cbs@gmail.com>
2022-06-28 23:43:33 +0200

    Swapped a few pieces of code to see if it can handle csv with 622 million rows.

--- CSV2ODS/Program.cs
+++ CSV2ODS/Program.cs
@@ -12,7 +12,7 @@ namespace TryCSV2ODSConvertion
         {
             Console.WriteLine("Process starts.");
 
-            var directory = @"D:\CBS_Work\TeamOpenData\TryCSV2ODSConvertion\Data\";
+            var directory = @"D:\CBS_Work\TeamOpenData\CSV2ODS\Data\";
             var tempDir = Path.Combine(directory, "tempCSV");
 
             string[] csvFiles = Directory.GetFiles(directory, "*.csv");
@@ -20,49 +20,54 @@ namespace TryCSV2ODSConvertion
             // Constant row count limit
             int MAX_ROW_COUNT = 1048576;
 
-            // Split the large csv file
-            foreach (string csvFile in csvFiles)
-            {
-                using (StreamReader stReader = new StreamReader(csvFile))
-                {
-                    int fileIndex = 0;
+            //// Split the large csv file
+            //foreach (string csvFile in csvFiles)
+            //{
+            //    using (StreamReader stReader = new StreamReader(csvFile))
+            //    {
+            //        int fileIndex = 0;
 
-                    while (!stReader.EndOfStream)
-                    {
-                        int count = 0;
-                        using (StreamWriter stWriter = new StreamWriter(Path.Combine(tempDir, fileIndex++ + Path.GetFileName(csvFile))))
-                        {
-                            stWriter.AutoFlush = true;
-                            while (!stReader.EndOfStream && ++count < MAX_ROW_COUNT)
-                            {
-                                stWriter.WriteLine(stReader.ReadLine());
-                            }
-                        }
-                    }
-                }
-            }
+            //        while (!stReader.EndOfStream)
+            //        {
+            //            int count = 0;
+            //            using (StreamWriter stWriter = new StreamWriter(Path.Combine(tempDir, fileIndex++ + Path.GetFileName(csvFile))))
+            //            {
+            //                stWriter.AutoFlush = true;
+            //                while (!stReader.EndOfStream && ++count < MAX_ROW_COUNT)
+            //                {
+            //                    stWriter.WriteLine(stReader.ReadLine());
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
 
             // Convert smaller temp csv files
-            string[] smallerCSVs = Directory.GetFiles(tempDir, "*.csv");
+            string[] inputCSVs = Directory.GetFiles(tempDir, "*.csv");
 
             string outputODS = Path.Combine(tempDir, "output.ods");
 
-            // load the csv file in an instance of Workbook
+            // the final big output 
             var destWorkbook = new Workbook();
-
-            WorksheetCollection sheets = destWorkbook.Worksheets;
-
-            for (int i = 0; i < smallerCSVs.Length; i++)
+            
+            for (int i = 0; i < inputCSVs.Length; i++)
             {
-                var tempWorkbook = new Workbook(smallerCSVs[i]);
+                WorksheetCollection sheets = destWorkbook.Worksheets;
+
+                var tempWorkbook = new Workbook(inputCSVs[i]);
 
                 sheets[i].Copy(tempWorkbook.Worksheets[0]);
-
+                
                 sheets.Add();
-            }
 
-            // save csv as ods
-            destWorkbook.Save(outputODS, Aspose.Cells.SaveFormat.Auto);
+                Console.WriteLine(DateTime.Now + ": sheets" + i.ToString() + " is added to the workbook.");
+
+                // save csv as ods
+                destWorkbook.Save(outputODS, SaveFormat.Ods);
+            }
+            
+            //// save csv as ods
+            //destWorkbook.Save(outputODS, SaveFormat.Ods);
 
             Console.WriteLine("Process ends.");
             Console.Read();
